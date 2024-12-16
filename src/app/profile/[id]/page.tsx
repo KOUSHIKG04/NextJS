7cb497fe10acd4661ebc877c1c83c8ab8ac21d56
KOUSHIKG04 <koushik.20201ise0024@presidencyuniversity.in>
2024-12-16 16:51:57 +0530

    Add correction for vercel deployement

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,17 +1,22 @@
-export default function UserProfile({ params }: { params: { id: string } }) {
+import { FC } from "react";
 
-  
+// Correctly typing the props for Next.js Page
+interface UserProfileProps {
+  params: {
+    id: string;
+  };
+}
 
+const UserProfile: FC<UserProfileProps> = ({ params }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      
       <hr />
       <p className="text-2xl font-normal">
-        YOUR PROFILE ID = 
-        <span className="p-2 text-black font-bold">
-           {params.id}
-        </span>
+        YOUR PROFILE ID =
+        <span className="p-2 text-black font-bold">{params.id}</span>
       </p>
     </div>
   );
-}
+};
+
+export default UserProfile;
