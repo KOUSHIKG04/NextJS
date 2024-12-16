a9b8ed723c9f8fbb078f7b9e3fc13fa71701bb99
KOUSHIKG04 <koushik.20201ise0024@presidencyuniversity.in>
2024-12-16 17:41:50 +0530

    More Correction!

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,19 +1,15 @@
-// src/app/profile/[id]/page.tsx
-import { FC } from "react";
 
-interface UserProfileProps {
-  params: {
-    id: string;
-  };
-}
 
-const UserProfile: FC<UserProfileProps> = ({ params }) => {
+type tParams = Promise<{ id: string }>;
+
+const UserProfile = async ({ params }: { params: tParams }) => {
+  const { id } = await params; // Await the promise to resolve the params
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <hr />
       <p className="text-2xl font-normal">
-        YOUR PROFILE ID =
-        <span className="p-2 text-black font-bold">{params.id}</span>
+        YOUR PROFILE ID =<span className="p-2 text-black font-bold">{id}</span>
       </p>
     </div>
   );
