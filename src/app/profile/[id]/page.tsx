166c36aadb270c105dc0bccfdca41054277b6de7
KOUSHIKG04 <koushik.20201ise0024@presidencyuniversity.in>
2024-12-16 17:06:57 +0530

    Resolved merge conflict in [id]/page.tsx

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
 
-=======
-import { FC } from "react";
-
-// Correctly typing the props for Next.js Page
->>>>>>> 7cb497fe10acd4661ebc877c1c83c8ab8ac21d56
 interface UserProfileProps {
   params: {
     id: string;
   };
 }
 
-<<<<<<< HEAD
+
 const UserProfile = ({ params } : UserProfileProps) => {
-=======
-const UserProfile: FC<UserProfileProps> = ({ params }) => {
->>>>>>> 7cb497fe10acd4661ebc877c1c83c8ab8ac21d56
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <hr />
