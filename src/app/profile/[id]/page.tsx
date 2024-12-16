5838ad444886af4799818ef7530505969cfd5dd6
KOUSHIKG04 <koushik.20201ise0024@presidencyuniversity.in>
2024-12-16 16:57:25 +0530

    Add correction for vercel deployement

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,17 +1,20 @@
-export default function UserProfile({ params }: { params: { id: string } }) {
 
-  
+interface UserProfileProps {
+  params: {
+    id: string;
+  };
+}
 
+const UserProfile = ({ params } : UserProfileProps) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      
       <hr />
       <p className="text-2xl font-normal">
-        YOUR PROFILE ID = 
-        <span className="p-2 text-black font-bold">
-           {params.id}
-        </span>
+        YOUR PROFILE ID =
+        <span className="p-2 text-black font-bold">{params.id}</span>
       </p>
     </div>
   );
-}
+};
+
+export default UserProfile;
